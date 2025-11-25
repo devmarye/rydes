72c57d422e2e709ea3fd99ae763485f37905598c
brendagold <petersbrenda47@gmail.com>
2025-11-25 16:28:20 -0500

    adds tfstate to gitignore files

--- IaC/main.tf
+++ IaC/main.tf
@@ -1,5 +1,31 @@
 resource "aws_amplify_app" "rydes" {
   name       = "rydes"
-  repository = "https://github.com/rydes/website"
+  repository = "https://github.com/devmarye/rydes"
+  access_token = var.github_token
 
+   # Build spec for website in subfolder
+ build_spec = <<EOF
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands: []
+    build:
+      commands: []
+  artifacts:
+    baseDirectory: website  
+      - '**/*'
+  cache:
+    paths: []
+EOF
+
+  environment_variables = {
+    ENV = "prod"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.rydes.id
+  branch_name = "main"
+  enable_auto_build = true
 }
