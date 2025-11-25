5715f18ef0294bf2f96d9c92ea976f687689de27
Chukwuma Mary <petersbrenda47@gmail.com>
2025-11-25 12:14:07 -0500

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'ca-central-1_VvhVXztyw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3okget7ffg8q3vbvqnoimqlktd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '718ei0grau17ls4ojlu152trj1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
