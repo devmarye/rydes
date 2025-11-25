c5d4280c010a4ff68dc8673ea573eff36943910c
Chukwuma Mary <petersbrenda47@gmail.com>
2025-11-25 12:19:15 -0500

    Change userPoolClientId in config.js
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'ca-central-1_VvhVXztyw', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '718ei0grau17ls4ojlu152trj1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '22922fkr0hohnr13e1cs2j9b6s', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
