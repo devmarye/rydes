96ad2c71da40b5779d1101d1040a5f66ee7e407d
brendagold <petersbrenda47@gmail.com>
2025-11-26 11:18:12 -0500

    adds DynamoDB table and IAM roles for Lambda function; configures Lambda function with code archive

--- IaC/main.tf
+++ IaC/main.tf
@@ -82,3 +82,76 @@ resource "aws_cognito_user_pool_domain" "default_domain" {
   domain       = "rydes-spa-demo" 
   user_pool_id = aws_cognito_user_pool.main_pool.id
 }
+
+resource "aws_dynamodb_table" "rydes-table" {
+  name           = "rydes-table"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "RideId"
+
+  attribute {
+    name = "RideId"
+    type = "S"
+  }
+
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "rydes-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_dynamodb_policy" {
+  name        = "rydes-lambda-dynamodb-policy"
+  description = "Allows Lambda to write to DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem"
+        ]
+        Resource = aws_dynamodb_table.rydes-table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_dynamodb_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "archive_file" "requestRydes_code" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/requestRydes.js"
+  output_path = "${path.module}/lambda/requestRydes.zip"
+}
+
+resource "aws_lambda_function" "myfunc" {
+  filename         = data.archive_file.requestRydes_code.output_path
+  source_code_hash = data.archive_file.requestRydes_code.output_base64sha256
+  function_name    = "requestRydes"
+  role             = aws_iam_role.lambda_role.arn
+  handler          =  "requestRydes.handler"
+  runtime          = "nodejs18.x"
+  
+}
